9398c066a136fe9463626aac5ff9a83802e91a91
Arshia Aamena <121088748+arshia1234567@users.noreply.github.com>
2025-01-29 17:38:20 +0000

    updated

--- portfolio.py
+++ portfolio.py
@@ -118,7 +118,7 @@ with col1:
     <div class="contact-info">
         📍 Machilipatnam<br>
         📧 arshiaa245@gmail.com<br>
-        
+        <br>
         <a href="https://linkedin.com/in/arshia-aamena" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"/></a>
         <a href="https://github.com/arshia1234567" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"/></a>
     </div>
